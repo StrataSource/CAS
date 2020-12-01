e6c8dbc8be0c9ad2e20bd53b15c9cd64bdbb2a5c
Alex Zero <joseph@marsden.space>
2020-12-01 05:48:31 +0000

    refactor: add publish dependencies, bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="cas",
-    version="1.0.1",
+    version="1.0.2",
     description="Chaos Automation System",
     url="https://github.com/ChaosInitiative/CAS",
     author="Chaos Initiative",
